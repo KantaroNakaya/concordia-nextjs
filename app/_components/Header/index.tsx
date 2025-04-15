5968e9cdae0ad68531866de0c31bd8bf269da0dd
KantaroNakaya <shimeno38saiko@gmail.com>
2025-04-15 10:08:02 +0900

    ヘッダー背景色　スクロールで不透明度を更新

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,15 +1,30 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import cx from "classnames";
 import styles from "./index.module.css";
 import DesktopMenu from "@/app/_components/DesktopMenu";
 import HamMenu from "@/app/_components/HamMenu";
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const mainVisual = document.querySelector("main > div:first-child");
+            if (mainVisual) {
+                const mainVisualBottom =
+                    mainVisual.getBoundingClientRect().bottom;
+                setIsScrolled(mainVisualBottom < 0);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
     return (
-        <div className={styles.menuBox}>
+        <div className={cx(styles.menuBox, { [styles.scrolled]: isScrolled })}>
             <div className={styles.menuLogo}>
                 <a href="/">CONCORDIA</a>
             </div>
