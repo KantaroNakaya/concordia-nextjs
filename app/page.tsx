8718a0255c9aac7c4727a8dd72aee633c9357682
KantaroNakaya <shimeno38saiko@gmail.com>
2025-04-17 11:04:48 +0900

    トップページ　mv下にテキスト追加

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,33 @@ export default async function Home() {
         <>
             <section className={styles.topSection}>
                 <AnimatedSection>
+                    <div className={styles.heroText}>
+                        <p className={styles.heroTitle}>便利と自然</p>
+                        <p className={styles.heroSubtitle}>
+                            相反する２つの概念が両立する
+                        </p>
+                        <p className={styles.heroDescription}>
+                            圧倒的なアクセスの良さによる
+                            <br />
+                            充実した時間を過ごしつつ
+                            <br />
+                            ひとまずホテルに戻ると
+                            <br />
+                            緑に囲まれ開放的な空間が味わえる
+                        </p>
+                        <p className={styles.heroMessage}>
+                            「忙しい人のための最高の休日を
+                            <br />
+                            あなたに授けたい」
+                        </p>
+                        <p className={styles.heroConclusion}>
+                            これが私たちCONCORDIAの願いです
+                        </p>
+                    </div>
+                </AnimatedSection>
+            </section>
+            <section className={styles.topSection}>
+                <AnimatedSection>
                     <h2 className={styles.topSectionTitle}>CHARM</h2>
                     <div className={styles.charmBox}>
                         <div className={styles.charm}>
