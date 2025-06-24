2dfc0cb76acef61c370fca5e8f7beabcc922f879
KantaroNakaya <shimeno38saiko@gmail.com>
2025-06-24 11:34:45 +0900

    TopRoomコンポーネントの画像　RoomListコンポーネントの画像と同じものにする

--- app/_components/ImageSwiper/index.tsx
+++ app/_components/ImageSwiper/index.tsx
@@ -1,34 +1,33 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay,EffectFade } from "swiper/modules";
+import { Autoplay, EffectFade } from "swiper/modules";
 import Image from "next/image";
+import type { Room } from "@/app/_libs/microcms";
 
 // Swiperのスタイルをインポート
 import "swiper/css";
 import "swiper/css/autoplay";
 import "swiper/css/effect-fade";
+
 type ImageSwiperProps = {
-    images: {
-        src: string;
-        alt: string;
-    }[];
+    rooms: Room[];
 };
 
-export default function ImageSwiper({ images }: ImageSwiperProps) {
+export default function ImageSwiper({ rooms }: ImageSwiperProps) {
     return (
         <Swiper
-            modules={[ Autoplay, EffectFade ]}
+            modules={[Autoplay, EffectFade]}
             spaceBetween={30}
             slidesPerView={1}
             autoplay={{ delay: 2500, disableOnInteraction: false }}
             effect="fade"
         >
-            {images.map((image, index) => (
+            {rooms.map((room, index) => (
                 <SwiperSlide key={index}>
                     <Image
-                        src={image.src}
-                        alt={image.alt}
+                        src={room.image.url}
+                        alt={room.name}
                         style={{ width: "100%", height: "auto" }}
                         width={500}
                         height={281}
--- app/_components/TopRoom/index.tsx
+++ app/_components/TopRoom/index.tsx
@@ -1,13 +1,18 @@
 import styles from "./index.module.css";
 import ImageSwiper from "@/app/_components/ImageSwiper";
 import ButtonLink from "@/app/_components/ButtonLink";
-import { roomImages } from "@/app/_constants/images";
+import { getRoomsList } from "@/app/_libs/microcms";
+import { ROOMS_LIST_LIMIT } from "@/app/_constants";
 
-export default function TopRoom() {
+export default async function TopRoom() {
+    const { contents: roomsItems } = await getRoomsList({
+        limit: ROOMS_LIST_LIMIT,
+    });
+
     return (
         <div>
             <div className={styles.wrapper}>
-                <ImageSwiper images={roomImages} />
+                <ImageSwiper rooms={roomsItems} />
                 <p className={styles.text}>
                     部屋は日差しの入る間取りとなっており、大阪中心地に居ながら自然な気持ちで快適に過ごすことができます。
                 </p>
